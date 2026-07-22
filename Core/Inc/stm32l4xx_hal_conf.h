fc30a930606e2f1d523e7c8166bfcef8760941e4
Dark Tyr <johnathon.gard@colorado.edu>
2026-07-22 16:13:22 -0600

    Update LSE Startup timeout to one second to prevent boot lock-up.

--- Core/Inc/stm32l4xx_hal_conf.h
+++ Core/Inc/stm32l4xx_hal_conf.h
@@ -152,7 +152,7 @@
 #endif /* LSE_VALUE */
 
 #if !defined  (LSE_STARTUP_TIMEOUT)
-  #define LSE_STARTUP_TIMEOUT    100U   /*!< Time out for LSE start up, in ms */
+  #define LSE_STARTUP_TIMEOUT    1000U   /*!< Time out for LSE start up, in ms */
 #endif /* HSE_STARTUP_TIMEOUT */
 
 /**
